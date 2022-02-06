906e6e2670db5673ec0bcc7d9b7717634b3b4971
Khaled DERKAOUI <khaledderkaoui22@gmail.com>
2022-02-06 21:02:10 +0100

    just to learn

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -58,7 +58,7 @@ const FeedbackForm = () => {
     return ( 
         <Card>
             <form onSubmit={handleSubmit}>
-                <h2>How would you rate your service with us?</h2>
+                <h1>How would you rate your service with us ?</h1>
                 <Rating handleRating={ (rt)=>setRating(rt) }/>
                 <div className="input-group">
                     <input type="text" placeholder="Wite a review" onChange={handelTextChange} value={text} />
